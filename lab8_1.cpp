d2eec5ec0674a9de03a3cb57ffdba9074d50dc20
Bank5807 <tanat_wip@cmu.ac.th>
2020-01-24 16:23:11 +0700

    lab8 v1

--- lab8_1.cpp
+++ lab8_1.cpp
@@ -2,29 +2,52 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
 
-	//use 'setw' to set width of table and 'left' to set left-alignment
-	//you can change input argument of 'setw()' to see the effect
-	//Try to change from 'left' to 'right' and see the effect
-	cout << setw(13) << left << "EndOfYear#"; 
-	cout << setw(13) << left << "PrevBalance"; 
-	cout << setw(13) << left << "Interest"; 
+    double loan, rate, pay;
+    cout << "Enter initial loan: ";
+    cin >> loan;
+    cout << "Enter interest rate per year(%): ";
+    cin >> rate;
+    cout << "Enter amount you can pay per year: ";
+    cin >> pay;
+
+	cout << setw(13) << left << "EndOfYear#";
+	cout << setw(13) << left << "PrevBalance";
+	cout << setw(13) << left << "Interest";
 	cout << setw(13) << left << "Total";
 	cout << setw(13) << left << "Payment";
 	cout << setw(13) << left << "NewBalance";
 	cout << "\n";
-	
-	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
-	//you can change input argument of 'setprecision()' to see the effect
-	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
-	
+
+	int year = 1;
+	double inter = 0, payment = 0, total = 0;
+
+	do{
+	cout << fixed << setprecision(2);
+	cout << setw(13) << left << year++;
+	cout << setw(13) << left << loan;
+
+	inter = loan * (rate/100);
+    if(inter > pay){
+        cout << "you can't pay with this amount of monny.";
+        break;
+    }
+    total = loan + inter;
+	cout << setw(13) << left << inter;
+	cout << setw(13) << left << total;
+
+    if(loan < pay){
+        payment = total;
+    }else{
+        payment = pay;
+    }
+	cout << setw(13) << left << payment;
+
+	loan = total - payment;
+	cout << setw(13) << left << loan;
+	cout << "\n";
+	}while(loan > 0);
+
 	return 0;
 }
--- lab8_2.cpp
+++ lab8_2.cpp
@@ -1,24 +1,37 @@
 #include<iostream>
 using namespace std;
 
-//Write the function printO() here
+string printO(int M, int N){
+    int i, j;
+    string x = "";
+    if(M <= 0 || N <= 0){
+        cout << "Invalid input\n";
+    }
+    for(j = 1; j <= M;j++){
+        for(i = 1; i <= N;i++){
+            cout << "O";
+            }
+        cout << "\n";
+        }
+    return x;
+    }
 
 int main(){
-	
+
 	printO(2,2);
 	cout << "\n";
-	
+
 	printO(3,5);
-	cout << "\n";	
-	
+	cout << "\n";
+
 	printO(5,3);
-	cout << "\n";	
-	
+	cout << "\n";
+
 	printO(0,3);
-	cout << "\n";	
-	
+	cout << "\n";
+
 	printO(7,-1);
-	cout << "\n";	
-	
+	cout << "\n";
+
 	return 0;
 }
